3d136b2b2eac61f162294fbc7a20b4a207433b52
hkoma2jp <h.koma2.jp@gmail.com>
2020-10-21 17:29:50 +0800

    #1 reset shared heading styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,24 +9,8 @@ export default () => (
   <>
     <Header titlePre="Home" />
     <div className={sharedStyles.layout}>
-      <img
-        src="/vercel-and-notion.png"
-        height="85"
-        width="250"
-        alt="Vercel + Notion"
-      />
-      <h1>My Notion Blog</h1>
-      <h2>
-        Blazing Fast Notion Blog with Next.js'{' '}
-        <ExtLink
-          href="https://github.com/vercel/next.js/issues/9524"
-          className="dotted"
-          style={{ color: 'inherit' }}
-        >
-          SSG
-        </ExtLink>
-      </h2>
-
+      <h1>Blog Name</h1>
+      <h2>Sub Name</h2>
       <Features />
     </div>
   </>
