261d83caa2b69129f20a314533bc5f074e105ab1
hkoma2jp <h.koma2.jp@gmail.com>
2020-12-12 18:08:05 +0800

    #1 add footer styles

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,12 @@
+import styles from '../styles/footer.module.css'
 import ExtLink from './ext-link'
 
 export default () => (
   <>
-    <footer>footer</footer>
+    <footer className={styles.footer}>
+      <p className={styles.copy}>
+        Copyright &copy; hkoma2jp All Rights Reserved.
+      </p>
+    </footer>
   </>
 )
