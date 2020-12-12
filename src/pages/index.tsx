53820a3057fcf622291bc49667c78402981fbcf9
hkoma2jp <h.koma2.jp@gmail.com>
2020-12-12 18:21:01 +0800

    #1 fix top styles and shared styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import Link from 'next/link'
 import Header from '../components/header'
 import ExtLink from '../components/ext-link'
-import Features from '../components/features'
 import GitHub from '../components/svgs/github'
 import sharedStyles from '../styles/shared.module.css'
 
@@ -9,9 +8,7 @@ export default () => (
   <>
     <Header titlePre="Home" />
     <div className={sharedStyles.layout}>
-      <h1>Blog Name</h1>
-      <h2>Sub Name</h2>
-      <Features />
+      <h1>Welcome</h1>
     </div>
   </>
 )
