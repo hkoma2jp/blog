5fc9cce1c76f61d0278e2acf0a2b4d105af750be
hkoma2jp <h.koma2.jp@gmail.com>
2020-12-12 18:27:17 +0800

    #1 fix contact styles

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -13,22 +13,17 @@ const contacts = [
   {
     Comp: Twitter,
     alt: 'twitter icon',
-    link: 'https://twitter.com/_ijjk',
+    link: 'https://twitter.com/@@@@@@@@@@',
   },
   {
     Comp: GitHub,
     alt: 'github icon',
-    link: 'https://github.com/ijjk',
-  },
-  {
-    Comp: LinkedIn,
-    alt: 'linkedin icon',
-    link: 'https://www.linkedin.com/in/jj-kasper-0b5392166/',
+    link: 'https://github.com/@@@@@@@@@@',
   },
   {
     Comp: Envelope,
     alt: 'envelope icon',
-    link: 'mailto:jj@jjsweb.site?subject=Notion Blog',
+    link: 'mailto:@@@@@@@@@@',
   },
 ]
 
@@ -37,14 +32,13 @@ export default () => (
     <Header titlePre="Contact" />
     <div className={sharedStyles.layout}>
       <div className={contactStyles.avatar}>
-        <img src="/avatar.png" alt="avatar with letters JJ" height={60} />
+        {/* <img src="/avatar.png" alt="avatar with letters JJ" height={60} /> */}
       </div>
 
       <h1 style={{ marginTop: 0 }}>Contact</h1>
 
       <div className={contactStyles.name}>
-        JJ Kasper - Next.js Engineer @{' '}
-        <ExtLink href="https://vercel.com">Vercel</ExtLink>
+        @ <ExtLink href="https://twitter.com/@@@@@@@@@@">@@@@@@@@@@</ExtLink>
       </div>
 
       <div className={contactStyles.links}>
