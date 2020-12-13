61030b3220ea276b81bf361a748ee6cc842dbd7c
hkoma2jp <h.koma2.jp@gmail.com>
2020-12-13 20:29:49 +0800

    #3 Google Tag Manager の設置

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,24 @@
+import { FC, useEffect } from 'react'
+import { AppProps } from 'next/app'
 import '../styles/global.css'
 import '../styles/footer.module.css'
 import 'katex/dist/katex.css'
 import Footer from '../components/footer'
+import TagManager from 'react-gtm-module'
 
-export default ({ Component, pageProps }) => (
-  <>
-    <div className="wrap">
-      <Component {...pageProps} />
-      <Footer />
-    </div>
-  </>
-)
+const AppComponent: FC<AppProps> = ({ Component, pageProps }: AppProps) => {
+  // Google Tag Manager
+  useEffect(() => {
+    TagManager.initialize({ gtmId: 'GTM-KBCTQJ7' })
+  }, [])
+
+  return (
+    <>
+      <div className="wrap">
+        <Component {...pageProps} />
+        <Footer />
+      </div>
+    </>
+  )
+}
+export default AppComponent
